0bf75881f16cc384dbc59aac870105a5b3a534e8
davidfrp <davidfrp@outlook.dk>
2021-06-29 16:03:53 +0200

    Initial commit

---
+++ Depiction.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.UI.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+
+namespace DepictionsApp
+{
+    internal class Depiction
+    {
+        public double ZoomPercentage { get; private set; }
+
+        // TODO: Clamp location by "boxing" it in.
+        public Point Location { get; set; }
+
+        // TODO: Clamp size.
+        public Size Size { get; set; }
+
+        private CanvasControl _canvas;
+        private CanvasBitmap _sourceImage;
+
+        public Depiction(CanvasControl canvas, CanvasBitmap sourceImage)
+        {
+            _canvas = canvas;
+            _sourceImage = sourceImage;
+
+            this.Size = _sourceImage.Size;
+            this.ZoomPercentage = 100;
+        }
+
+        public void MoveDrawnImage(Point previousDraggingPoint, Point currentDraggingPoint)
+        {
+            Vector2 displacementVector = new Vector2(
+                    (float)currentDraggingPoint.X - (float)previousDraggingPoint.X,
+                    (float)currentDraggingPoint.Y - (float)previousDraggingPoint.Y
+                );
+
+            MoveDrawnImage(displacementVector);
+        }
+
+        public void MoveDrawnImage(Vector2 displacementVector)
+        {
+            this.Location = new Point(
+                    this.Location.X + displacementVector.X,
+                    this.Location.Y + displacementVector.Y
+                );
+
+            _canvas.Invalidate();
+        }
+
+        public void RevertDrawnImage()
+        {
+            this.Location = new Point();
+            this.Size = _sourceImage.Size;
+            _canvas.Invalidate();
+        }
+
+        public void FillDrawnImageInCanvas(bool allowOverflow, bool useSourceImageAsMaxSize)
+        {
+            double widthZoomFactor = _canvas.Width / _sourceImage.Size.Width;
+            double heightZoomFactor = _canvas.Height / _sourceImage.Size.Height;
+
+            double zoomFactor = widthZoomFactor < heightZoomFactor ? widthZoomFactor : heightZoomFactor;
+
+            if (allowOverflow)
+                zoomFactor = widthZoomFactor > heightZoomFactor ? widthZoomFactor : heightZoomFactor;
+
+            // Ensure image doesn't get scaled beyond its source's size.
+            if (useSourceImageAsMaxSize && zoomFactor > 1)
+                zoomFactor = 1;
+
+            ZoomDrawnImage(zoomFactor * 100);
+
+            CenterDrawnImage();
+        }
+
+        public void CenterDrawnImage()
+        {
+            Point centerPoint = GetCanvasCenterPoint();
+
+            this.Location = new Point(
+                    centerPoint.X - this.Size.Width / 2,
+                    centerPoint.Y - this.Size.Height / 2
+                );
+
+            _canvas.Invalidate();
+        }
+
+        public Point GetCanvasCenterPoint()
+        {
+            return new Point(_canvas.Width / 2, _canvas.Height / 2);
+        }
+
+        public void ZoomDrawnImage(double zoomPercentage)
+        {
+            ZoomDrawnImage(zoomPercentage, GetCanvasCenterPoint());
+        }
+
+        public void ZoomDrawnImage(double zoomPercentage, Point fixedPoint)
+        {
+            // TODO: Attempt to rewrite zoom functionallity.
+
+            // Place image so the fixed pixel is below the mouse cursor.
+            Point fixedPixel = PointFromCanvasToSourceImage(fixedPoint);
+
+            Point newLocation = new Point(
+                    0,
+                    0
+                );
+
+            this.Location = newLocation;
+
+
+
+            Size newSize = new Size(
+                    zoomPercentage / 100 * this.Size.Width,
+                    zoomPercentage / 100 * this.Size.Height
+                );
+
+            this.Size = newSize;
+
+            _canvas.Invalidate();
+        }
+
+        private Point PointFromCanvasToDrawnImage(Point pointOnCanvas)
+        {
+            return new Point(
+                    pointOnCanvas.X - this.Location.X,
+                    pointOnCanvas.Y - this.Location.Y
+                );
+        }
+
+        private Point PointFromCanvasToSourceImage(Point pointOnCanvas)
+        {
+            double widthScaleFactor = this.Size.Width / _sourceImage.Size.Width;
+            double heightScaleFactor = this.Size.Height / _sourceImage.Size.Height;
+
+            double imagePositionX = PointFromCanvasToDrawnImage(pointOnCanvas).X / widthScaleFactor;
+            double imagePositionY = PointFromCanvasToDrawnImage(pointOnCanvas).Y / heightScaleFactor;
+
+            return new Point(imagePositionX, imagePositionY);
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.UI.Xaml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
+using Windows.UI.Core;
+using Windows.UI.Input;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace DepictionsApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public CanvasBitmap SourceImage { get; set; }
+
+        private Depiction _depiction;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            FileOpenPicker fileOpenPicker = new FileOpenPicker();
+            fileOpenPicker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+            fileOpenPicker.FileTypeFilter.Add(".jpg");
+            fileOpenPicker.FileTypeFilter.Add(".jpeg");
+            fileOpenPicker.FileTypeFilter.Add(".jpe");
+            fileOpenPicker.FileTypeFilter.Add(".jfif");
+            fileOpenPicker.FileTypeFilter.Add(".png");
+                
+            StorageFile file = await fileOpenPicker.PickSingleFileAsync();
+            //StorageFolder storageFolder = KnownFolders.MusicLibrary;
+
+            if (file != null)
+                await LoadSourceImageAsync(file);
+
+            //MessageDialog messageDialog = new MessageDialog($"{this.SourceImage?.Bounds.Width}x{this.SourceImage?.Bounds.Height}", "Image properties");
+            //messageDialog.ShowAsync();
+        }
+
+        private void canvas_Draw(CanvasControl sender, CanvasDrawEventArgs args)
+        {
+            if (this.SourceImage != null)
+            {
+                args.DrawingSession.DrawImage(this.SourceImage,
+                        new Rect(
+                                _depiction.Location.X,
+                                _depiction.Location.Y,
+                                _depiction.Size.Width,
+                                _depiction.Size.Height
+                            )
+                    );
+            }
+        }
+
+        private async Task LoadSourceImageAsync(StorageFile file)
+        {
+            using (IRandomAccessStream stream = await file.OpenReadAsync())
+            {
+                this.SourceImage = await CanvasBitmap.LoadAsync(canvas, stream);
+
+                _depiction = new Depiction(canvas, this.SourceImage);
+            }
+
+            canvas.Invalidate();
+        }
+
+        private Point _previousDraggingPoint;
+        private bool _isMovingImage;
+
+        private void canvas_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            ((CanvasControl)sender).CapturePointer(e.Pointer);
+
+            _previousDraggingPoint = e.GetCurrentPoint(sender as UIElement).Position;
+
+            _isMovingImage = true;
+        }
+
+        private void canvas_PointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            ((CanvasControl)sender).ReleasePointerCapture(e.Pointer);
+
+            _isMovingImage = false;
+        }
+
+        private void canvas_PointerMoved(object sender, PointerRoutedEventArgs e)
+        {
+            Point currentDraggingPoint = e.GetCurrentPoint(sender as UIElement).Position;
+
+            if (_isMovingImage)
+                _depiction.MoveDrawnImage(_previousDraggingPoint, currentDraggingPoint);
+
+            _previousDraggingPoint = currentDraggingPoint;
+        }
+
+        private void canvas_PointerWheelChanged(object sender, PointerRoutedEventArgs e)
+        {
+            PointerPoint currentPointerPoint = e.GetCurrentPoint(sender as UIElement);
+
+            // Determine whether the user zooms in or out, depending on the wheel direction.
+            bool isZoomingIn = currentPointerPoint.Properties.MouseWheelDelta > 0;
+
+            if (isZoomingIn)
+            {
+                // Increase zoom by 10%
+                _depiction.ZoomDrawnImage(1.1 * _depiction.ZoomPercentage, currentPointerPoint.Position);
+            }
+            else
+            {
+                // Decrease zoom by 10%
+                _depiction.ZoomDrawnImage(0.9 * _depiction.ZoomPercentage, currentPointerPoint.Position);
+            }
+        }
+    }
+}
